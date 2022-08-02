af9bdbcf79b02ec912ac6dca194dc22b0c6908fb
JanatB <janatbaig2@gmail.com>
2022-08-02 12:54:39 -0400

    implemented instances + methods

--- Java/Lox/Interpreter.java
+++ Java/Lox/Interpreter.java
@@ -42,7 +42,47 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     } catch (RuntimeError error) {
       Lox.runtimeError(error);
     }
-  } 
+  }  
+
+  @Override 
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+
+    if (!(object instanceof LoxInstance)) {
+      throw new RuntimeError(expr.name,  "Only instances have fields.");
+    } 
+
+    Object value = evaluate(expr.value);
+    ((LoxInstance)object).set(expr.name, value);
+    return value;
+
+  }
+
+  @Override 
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance)object).get(expr.name);
+    } 
+
+    throw new RuntimeError(expr.name, "Only instances have properties.");
+  }
+
+  @Override 
+  public Void visitClassStmt(Stmt.Class stmt) {
+    environment.define(stmt.name.lexeme, null);
+
+    Map<String, LoxFunction> methods = new HashMap<>();
+    for (Stmt.Function method : stmt.methods) {
+      LoxFunction function = new LoxFunction(method, environment);
+      methods.put(method.name.lexeme, function);
+    }
+
+    LoxClass klass = new LoxClass(stmt.name.lexeme, methods);
+    environment.assign(stmt.name, klass);
+    return null;
+
+  }
 
   @Override 
   public Void visitReturnStmt(Stmt.Return stmt) {
--- Java/Lox/Resolver.java
+++ Java/Lox/Resolver.java
@@ -19,7 +19,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum FunctionType {
         NONE, 
-        FUNCTION
+        FUNCTION,
+        METHOD
     }
 
     @Override 
@@ -145,6 +146,33 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         resolve(expr.right);
         return null;
     }
+
+    @Override 
+    public Void visitClassStmt(Stmt.Class stmt) {
+        declare(stmt.name);
+        define(stmt.name);
+
+        for (Stmt.Function method : stmt.methods) {
+            FunctionType declaration =  FunctionType.METHOD;
+            resolveFunction(method, declaration);
+        }
+
+      return null;
+    } 
+
+    @Override 
+    public Void visitGetExpr(Expr.Get expr) {
+        resolve(expr.object);
+        return null;
+    } 
+
+    @Override 
+    public Void visitSetExpr(Expr.Set expr) {
+        resolve(expr.value);
+        resolve(expr.object); 
+        return null;
+    }
+  
 
     // Helper Methods
 
